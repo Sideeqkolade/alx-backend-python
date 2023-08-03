056adaddba3b120c9d1a3dabee35195720786a34
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-04 00:05:18 +0100

    task 0 done

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A script that adds as float and return as float
+"""
+
+def add(a: float, b: float) -> float:
+    """ a type-annotated function add that takes a float
+        a and a float b as arguments and returns their sum
+        as a float
+    """
+
+    return a + b
