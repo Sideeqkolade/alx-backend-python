cef5b4777e9764a07c229816fba25d9800ed16ec
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-04 02:38:37 +0100

    task 6 done

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -5,4 +5,8 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    return (lambda a: a * multiplier)
+    """ a type-annotated function make_multiplier that takes
+        a float multiplier as argument and returns a function
+        that multiplies a float by multiplier
+    """
+    return lambda a: a * multiplier
