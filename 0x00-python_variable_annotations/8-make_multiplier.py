6a715492700faca1f6d39af5d9e91a661fa38ede
Sideeqkolade <sideeqkolade@gmail.com>
2023-08-04 02:28:39 +0100

    task 8 done

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" A script that returns a function that multiplies a float
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    return lambda a: a * multiplier
